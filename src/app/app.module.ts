fbe30edec7533d30e291e8b2c3edb2e539876879
Hmugera <mugerahughes@gmail.com>
2020-07-08 23:15:21 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import {ColorChromeModule} from 'ngx-color/chrome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GeneratorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ColorChromeModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
